1253994d4158477f35fded44d5510389cf896dab
Monica <monica.k@spi-global.com>
2019-02-28 17:25:28 +0530

    create generateMessage utility

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const socketIO = require('socket.io');
 const http = require('http');
 
+const {generateMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
@@ -35,24 +36,26 @@ console.log('New user connected');
 //   console.log('createEmail',newEmail);
 // });
 
-socket.emit('newMessage', {
+socket.emit('newMessage', generateMessage('Admin','Welcome to the chat app')/*{
   from: 'Admin',
   text:'Welcome to the chat application',
   createdAt: new Date().getTime()
-});
+}*/);
 
 
-socket.broadcast.emit('newMessage', {
+socket.broadcast.emit('newMessage', generateMessage('Admin','New User joined')/* {
   from: 'Admin',
   text:'New User joined',
   createdAt: new Date().getTime()
-})
+}*/);
 
 
 
 socket.on('createMessage',(newMessage) => {
   console.log('createMessage',newMessage);
 
+
+io.emit('newMessage',generateMessage(newMessage.from,newMessage.text));
   // io.emit('newMessage',{
   //   from: newMessage.from,
   //   text:newMessage.text,
