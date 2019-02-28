df705b22d6f73aeb01f455f39d11753e5fa804a0
Monica <monica.k@spi-global.com>
2019-02-28 16:01:16 +0530

    Emit newMessage on createMessage

--- server/server.js
+++ server/server.js
@@ -14,8 +14,35 @@ var app = express();
 
 var server = http.createServer(app);
 var io = socketIO(server);
+
+
 io.on('connection', (socket) => {
 console.log('New user connected');
+// socket.emit('newEmail', {
+//   from:'monica.ria92@gmail.com',
+//   text:'Hey',
+//   createAt: 123
+// });
+
+
+// socket.emit('newMessage',{
+//   from:'ria92@gmail.com',
+//   text:'Hey ria',
+//   createdAt: 456
+// });
+
+// socket.on('createEmail',(newEmail) => {
+//   console.log('createEmail',newEmail);
+// });
+
+socket.on('createMessage',(newMessage) => {
+  console.log('createMessage',newMessage);
+  io.emit('newMessage',{
+    from: newMessage.from,
+    text:newMessage.text,
+    createdAt:new Date().getTime()
+  });
+});
 socket.on('disconnect',() => {
   console.log('User was disconnect');
 })
