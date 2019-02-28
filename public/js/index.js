ba93a6f442f169ac16cb373a5b6f4ba68f4f0351
Monica <monica.k@spi-global.com>
2019-02-28 16:02:23 +0530

    Emit newMessage on createMessage

---
+++ public/js/index.js
@@ -0,0 +1,25 @@
+var socket = io();
+socket.on('connect', function ()  {
+  console.log('Connected to server');
+  // socket.emit('createEmail',{
+  //   to:'moni.ken@rediffmail.com',
+  //   text:'hey moni'
+  // });
+
+  // socket.emit('createMessage',{
+  //   from:'sofi.ken@rediffmail.com',
+  //   text:'hey sofi'
+  // })
+});
+
+socket.on('disconnect', function ()  {
+  console.log('Disconnected from server');
+});
+
+// socket.on('newEmail',function (email) {
+//   console.log('New Email',email);
+// });
+
+socket.on('newMessage',function (email) {
+  console.log('New Message',email);
+});
