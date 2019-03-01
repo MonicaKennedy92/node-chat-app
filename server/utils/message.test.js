4d54dde5954140652bfe65b2c85be31e0809a7e8
Monica <monica.k@spi-global.com>
2019-03-01 18:04:26 +0530

    minor changes

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,30 +1,27 @@
 var expect = require('expect');
 
-var {generateMessage,generateLocationMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
   it('should generate correct message object', () => {
     var from = 'Jen';
     var text = 'Some message';
-    var message = generateMessage(from,text);
+    var message = generateMessage(from, text);
 
-    expect(typeof message.createAt).toBe('number');
-    expect(message).toInclude({
-      from,
-      text
-    });
+    expect(typeof message.createdAt).toBe('number');
+    expect(message).toInclude({from, text});
+  });
+});
 
-  })
-})
 describe('generateLocationMessage', () => {
   it('should generate correct location object', () => {
-    var from = 'Moni';
+    var from = 'Deb';
     var latitude = 15;
     var longitude = 19;
     var url = 'https://www.google.com/maps?q=15,19';
-    var message = generateLocationMessage(from,latitude,longitude);
+    var message = generateLocationMessage(from, latitude, longitude);
 
     expect(typeof message.createdAt).toBe('number');
-    expect(message).toInclude({from,url});
-  })
-})
+    expect(message).toInclude({from, url});
+  });
+});
